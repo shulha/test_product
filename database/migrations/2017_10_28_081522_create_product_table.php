b521d8772cbdee8c30feaf06bc692ebaa98b4134
shulha <shulhad@gmail.com>
2017-10-28 12:02:13 +0300

    (feat) create migration CreateProductTable and Model

---
+++ database/migrations/2017_10_28_081522_create_product_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->unsignedInteger('programId');
+            $table->string('programName');
+            $table->unsignedDecimal('price', 6, 2);
+            $table->char('currency', 3);
+            $table->text('description');
+            $table->string('manufacturer');
+            $table->unsignedBigInteger('ean');
+            $table->string('image');
+            $table->unsignedDecimal('priceOld', 6, 2);
+            $table->unsignedDecimal('shippingCosts', 6, 2);
+            $table->string('merchantCategory');
+            $table->string('merchantEmail');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
